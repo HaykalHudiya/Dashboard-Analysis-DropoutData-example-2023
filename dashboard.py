d51bd197384a38098f0ee0b09b07936874add16b
HaykalHudiya <118410894+HaykalHudiya@users.noreply.github.com>
2024-12-03 17:59:29 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -22,39 +22,6 @@ def create_filter_prov_df(df):
     filter_prov = df.sort_values(by="PAGU", ascending=True)
     return filter_prov
 
-# def make_choropleth(input_df, input_id, input_column, input_color_theme):
-#     # Membaca file GeoJSON lokal
-#     with open('indonesia-edit.geojson') as f:
-#         geojson_data = json.load(f)
-    
-#     # Pastikan Anda memeriksa apakah nama provinsi dalam geojson dan df cocok
-#     input_df[input_id] = input_df[input_id].str.lower()  # pastikan data df juga lowercase
-#      # Check which provinces in df are not in the GeoJSON
-#     missing_provs = input_df[~input_df[input_id].isin([feature['properties']['state'].lower() for feature in geojson_data['features']])]
-#     print("Missing Provinces:", missing_provs)
-    
-#     # Membuat choropleth map menggunakan Plotly Express
-#     choropleth = px.choropleth(input_df, 
-#                                 geojson=geojson_data,  # Menggunakan data GeoJSON lokal
-#                                 locations=input_id, 
-#                                 color=input_column, 
-#                                 color_continuous_scale=input_color_theme,
-#                                 range_color=(0, input_df[input_column].max()),
-#                                 scope="asia",
-#                                 labels={input_column: input_column}
-#                                )
-#     # Memperbarui layout untuk peta
-#     choropleth.update_geos(fitbounds="locations", visible=False)
-#     choropleth.update_layout(
-#         template='plotly_dark',
-#         plot_bgcolor='rgba(255, 255, 255, 0)',  # Latar belakang peta yang transparan
-#         paper_bgcolor='rgba(255, 255, 255, 0)',  # Latar belakang kertas yang transparan
-#         margin=dict(l=0, r=0, t=0, b=0),
-#         height=350
-#     )
-    
-#     return choropleth
-
 def display_map(df):
     # Membuat peta dengan pusat Indonesia
     map = folium.Map(location=[-6.1751, 106.8650], zoom_start=5, scrollWheelZoom=False, tiles='CartoDB positron')
@@ -127,34 +94,24 @@ plt.barh(filter_prov_df['PROVINSI'], filter_prov_df['PAGU'].astype(str).apply(la
 plt.xlabel('PAGU (Triliun)')
 plt.ylabel('Provinsi')
 plt.title('Pagu APBN 2023 per Provinsi')
-plt.xlim(left=0)
 plt.tight_layout()
-
 st.pyplot(plt)
 
-# all_df['PROVINSI'] = all_df['PROVINSI'].str.lower()
-# fig = make_choropleth(all_df, 'PROVINSI', 'Jumlah Sekolah SD', 'Viridis')
-# st.plotly_chart(fig)
-# Membaca file GeoJSON
 geojson_url = 'indonesia-edit.geojson'
-# Buat peta pusat Indonesia
+
 m = folium.Map(location=[-6.1751, 106.8650], zoom_start=5)
-# Pastikan kolom 'PROVINSI' di all_df sesuai dengan nama di GeoJSON
-# Menormalisasi data di DataFrame dan GeoJSON (jika perlu)
 all_df['PROVINSI'] = all_df['PROVINSI'].str.lower()
-# Tambahkan layer GeoJSON
+
 folium.Choropleth(
     geo_data=geojson_url,
     data=all_df,
     columns=['PROVINSI', 'Jumlah Sekolah SD'],
-    key_on="feature.properties.state",  # Pastikan 'state' sesuai dengan properti GeoJSON
+    key_on="feature.properties.state", 
     fill_color="YlGnBu",
     fill_opacity=0.7,
     line_opacity=0.2,
     legend_name="Jumlah Sekolah SD"
-).add_to(m)
-# Menyimpan peta ke file HTML
-# st.components.v1.html(m._repr_html_(), width=700, height=500)
-# Menampilkan peta
+)
+ 
 state_name = display_map(all_df)
 st.write(f'Provinsi yang dipilih: {state_name}')
