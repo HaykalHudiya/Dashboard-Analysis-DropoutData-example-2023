999ab5c7a3b715c62f9bd637005ba41ed877f241
HaykalHudiya <118410894+HaykalHudiya@users.noreply.github.com>
2024-12-03 19:55:29 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -149,7 +149,7 @@ state_name = display_map(all_df)
 st.write(f'Provinsi yang dipilih: {state_name}')
 
 datas = all_df.loc[all_df['PROVINSI'] == state_name, 'Jumlah Sekolah SD']
-donut_chart_less = make_donut(, 'Testo', 'red')
+donut_chart_less = make_donut(datas, 'Testo', 'red')
 
     migrations_col = st.columns((0.2, 1, 0.2))
     with migrations_col[1]:
