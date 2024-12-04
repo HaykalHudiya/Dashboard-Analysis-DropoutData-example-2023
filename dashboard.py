322e7421c909a5d3289f1e62a1be556755ef8653
HaykalHudiya <118410894+HaykalHudiya@users.noreply.github.com>
2024-12-04 21:46:23 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -23,28 +23,9 @@ def create_filter_prov_df(df):
     filter_prov = df.sort_values(by="PAGU", ascending=True)
     return filter_prov
 
-# Heatmap
-# def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
-#     heatmap = alt.Chart(input_df).mark_rect().encode(
-#             y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
-#             x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
-#             color=alt.Color(f'max({input_color}):Q',
-#                              legend=None,
-#                              scale=alt.Scale(scheme=input_color_theme)),
-#             stroke=alt.value('black'),
-#             strokeWidth=alt.value(0.25),
-#         ).properties(width=900
-#         ).configure_axis(
-#         labelFontSize=12,
-#         titleFontSize=12
-#         ) 
-#     # height=300
-#     return heatmap
-
-# Choropleth map
 def make_choropleth(input_df, input_id, input_column, input_color_theme):
     geojson_url = 'indonesia-edit.geojson';
-    choropleth = px.choropleth(input_df, locations=input_id.str.lower(), color=input_column, geojson=geojson_url,
+    choropleth = px.choropleth(input_df, locations=input_df[input_id].str.lower(), color=input_column, geojson=geojson_url,
                                color_continuous_scale=input_color_theme,
                                range_color=(0, input_df[input_column].max()),
                                scope="asia",
@@ -182,9 +163,6 @@ st.pyplot(plt)
 
 choropleth = make_choropleth(all_df, 'PROVINSI', 'Jumlah Sekolah SD', 'viridis')
 st.plotly_chart(choropleth, use_container_width=True)
-    
-# heatmap = make_heatmap(df_reshaped, 'year', 'states', 'population', selected_color_theme)
-#     st.altair_chart(heatmap, use_container_width=True)
 
 state_name = display_map(all_df)
 st.write(f'Provinsi yang dipilih: {state_name}')
